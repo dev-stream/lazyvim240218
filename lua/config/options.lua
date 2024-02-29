6539e51d2e1cccd1e69f11f16a0e2d2034dd4fb6
Ernesto dev-stream <ernesto.dev.stream@gmail.com>
2024-02-29 15:11:55 +0800

    options for msys2 posix environment

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,22 @@
 
 -- LF not CRLF
 vim.opt.fileformats = "unix,dos"
+vim.opt.swapfile = false -- Currently trying other vim Distros
+
+if vim.env.MSYSTEM == "UCRT64" then
+  -- msys2 posix environment !!
+  vim.opt.shell = "bash --login"
+  vim.opt.shellcmdflag = "-i -c"
+
+  -- Tested `which bash` and `ll` custom `ls` alias in ~/.bashrc
+  vim.opt.shellxquote = "("
+  vim.opt.shellxescape = "^"
+
+  vim.opt.shellquote = ""
+  vim.opt.shellpipe = "2>&1| tee"
+  vim.opt.shellredir = ">%s 2>&1"
+  vim.opt.shellslash = true -- Unfortunately it gives issues with the Obsidian plugin
+end
 
 if vim.g.neovide then
   vim.keymap.set({ "n", "v" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
